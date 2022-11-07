f5149d70c5d3f1184d825e1459da061e2725b2b3
Leandro Matias Suarez <leandro_colocdk@hotmail.com>
2022-11-07 18:22:35 -0300

    Ahora si express con nodemon

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var http = require("http");
+// var http = require("http");
 
-http.createServer(function(request,response){
+// http.createServer(function(request,response){
 
-    response.writeHead(200,{"Content-type":"text/plain"});
+//     response.writeHead(200,{"Content-type":"text/plain"});
 
-    response.end("Hola Node!");
-}).listen(5000);
+//     response.end("Hola Node!");
+// }).listen(5000);
 
-console.log('Servidor e la url http://127.0.0.1:5000');
+// console.log('Servidor e la url http://127.0.0.1:5000');
+
+// Servidor sin express
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hola Mundo desde express.');
+})
+
+app.listen(3000, function () {
+    console.log("Aplicacion de ejemplo, escuchando puerto 3000");
+})
