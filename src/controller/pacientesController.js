ef84ae32fd7c888c777817a47566a112e70ec80a
Leandro Matias Suarez <leandro_colocdk@hotmail.com>
2022-11-23 16:36:06 -0300

    Relacion paciente medico

--- src/controller/pacientesController.js
+++ src/controller/pacientesController.js
@@ -1,26 +1,68 @@
+
+const models = require('../database/models/index');
+
+
 module.exports = {
-    listar: (req, res) => {
+    listar: (req, res, next) => {
         try {
             res.json({
                 message: "Listado de Pacientes"
             })
         } catch (err) {
             console.log(err)
+            return next(err)
         }
     },
 
-    crear: (req, res) => {
+    crear: async (req, res, next) => {
 
-    },
-
-    listarInfo: (req, res) => {
-        const id = req.params.idPaciente;
         try {
+            const medico = await models.medico.findByPk(req.body.medicoId)
+            if (!medico){
+                return res.json({
+                    message: "No existe un medico con ese ID"
+                })
+            }
+            const paciente = await models.paciente.create(req.body);
+
+
+            await models.tratamiento.create({
+                pacienteId: paciente.id,
+                medicoId: req.body.medicoId
+            })
+
             res.json({
-                message: "Informacion del Paciente " + id
+                message: "Paciente creado con exito"
             })
         } catch (err) {
             console.log(err)
+            return next(err)
+        }
+    },
+
+    listarInfo: async (req, res, next) => {
+        try {
+            const id = req.params.idPaciente;
+            const paciente = await models.paciente.findOne({
+                where: {
+                    id: id
+                },
+                include: [{
+                    model: models.tratamiento,
+                    include: [{
+                        model: models.medico
+                    }]
+                }]
+            })
+            res.json({
+                success: true,
+                data: {
+                    paciente: paciente
+                }
+            })
+        } catch (err) {
+            console.log(err)
+            next(err)
         }
     },
 
