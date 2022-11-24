de5eb8c62f0d7c6090b10fe5cf68df479905a258
Leandro Matias Suarez <leandro_colocdk@hotmail.com>
2022-11-24 09:13:20 -0300

    seed bulkInsert

--- src/database/seeders/20221120210114-primeros_medicos.js
+++ src/database/seeders/20221120210114-primeros_medicos.js
@@ -6,7 +6,7 @@ const models = require("../models/index");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -16,107 +16,182 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-     return Promise.all([
-      models.medico.findOrCreate({
-        where: {
-          id: "1"
-        },
-        defaults: {
-          nombre: "Carolina",
-          apellido: "Figeira",
-          email: "carolinaF@email.com",
-          dni: 34973132,
-          especialidad: "pediatria",
-          tiempo_trabajando: 38
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "2"
-        },
-        defaults: {
-          nombre: "Mariela",
-          apellido: "Esposito",
-          email: "marielaesposito@email.com",
-          dni: 23973143,
-          especialidad: "Neurologia",
-          tiempo_trabajando: 42
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "3"
-        },
-        defaults: {
-          nombre: "Hugo",
-          apellido: "Ghiani",
-          email: "ghianihugo2@email.com",
-          dni: 21673143,
-          especialidad: "Alergia",
-          tiempo_trabajando: 22
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "4"
-        },
-        defaults: {
-          nombre: "Dolores",
-          apellido: "Gomez",
-          email: "dologomez@email.com",
-          dni: 21954143,
-          especialidad: "Endocrinologia",
-          tiempo_trabajando: 100
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "5"
-        },
-        defaults: {
-          nombre: "Emilio",
-          apellido: "Lupi",
-          email: "emilupi@email.com",
-          dni: 31973143,
-          especialidad: "Geriatria",
-          tiempo_trabajando: 56
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "6"
-        },
-        defaults: {
-          nombre: "Silvana",
-          apellido: "Montalvo",
-          email: "silmontalvo@email.com",
-          dni: 45986236,
-          especialidad: "Nefrologia",
-          tiempo_trabajando: 61
-        }
-      }),
-      models.medico.findOrCreate({
-        where: {
-          id: "7"
-        },
-        defaults: {
-          nombre: "Leonel",
-          apellido: "Gatti",
-          email: "leogatti@email.com",
-          dni: 31973143,
-          especialidad: "Nutricion",
-          tiempo_trabajando: 30
-        }
-      })
-    ])
+    await queryInterface.bulkInsert('medico', [
+      {
+        nombre: "Carolina",
+        apellido: "Figeira",
+        email: "carolinaF@email.com",
+        dni: 34973132,
+        especialidad: "pediatria",
+        tiempo_trabajando: 38,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Mariela",
+        apellido: "Esposito",
+        email: "marielaesposito@email.com",
+        dni: 23973143,
+        especialidad: "Neurologia",
+        tiempo_trabajando: 42,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Hugo",
+        apellido: "Ghiani",
+        email: "ghianihugo2@email.com",
+        dni: 21673143,
+        especialidad: "Alergia",
+        tiempo_trabajando: 22,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Dolores",
+        apellido: "Gomez",
+        email: "dologomez@email.com",
+        dni: 21954143,
+        especialidad: "Endocrinologia",
+        tiempo_trabajando: 100,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Emilio",
+        apellido: "Lupi",
+        email: "emilupi@email.com",
+        dni: 31973143,
+        especialidad: "Geriatria",
+        tiempo_trabajando: 56,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Silvana",
+        apellido: "Montalvo",
+        email: "silmontalvo@email.com",
+        dni: 45986236,
+        especialidad: "Nefrologia",
+        tiempo_trabajando: 61,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Leonel",
+        apellido: "Gatti",
+        email: "leogatti@email.com",
+        dni: 31973143,
+        especialidad: "Nutricion",
+        tiempo_trabajando: 30,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ], {});
+
+
+    //  return Promise.all([
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "1"
+    //     },
+    //     defaults: {
+    //       nombre: "Carolina",
+    //       apellido: "Figeira",
+    //       email: "carolinaF@email.com",
+    //       dni: 34973132,
+    //       especialidad: "pediatria",
+    //       tiempo_trabajando: 38
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "2"
+    //     },
+    //     defaults: {
+    //       nombre: "Mariela",
+    //       apellido: "Esposito",
+    //       email: "marielaesposito@email.com",
+    //       dni: 23973143,
+    //       especialidad: "Neurologia",
+    //       tiempo_trabajando: 42
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "3"
+    //     },
+    //     defaults: {
+    //       nombre: "Hugo",
+    //       apellido: "Ghiani",
+    //       email: "ghianihugo2@email.com",
+    //       dni: 21673143,
+    //       especialidad: "Alergia",
+    //       tiempo_trabajando: 22
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "4"
+    //     },
+    //     defaults: {
+    //       nombre: "Dolores",
+    //       apellido: "Gomez",
+    //       email: "dologomez@email.com",
+    //       dni: 21954143,
+    //       especialidad: "Endocrinologia",
+    //       tiempo_trabajando: 100
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "5"
+    //     },
+    //     defaults: {
+    //       nombre: "Emilio",
+    //       apellido: "Lupi",
+    //       email: "emilupi@email.com",
+    //       dni: 31973143,
+    //       especialidad: "Geriatria",
+    //       tiempo_trabajando: 56
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "6"
+    //     },
+    //     defaults: {
+    //       nombre: "Silvana",
+    //       apellido: "Montalvo",
+    //       email: "silmontalvo@email.com",
+    //       dni: 45986236,
+    //       especialidad: "Nefrologia",
+    //       tiempo_trabajando: 61
+    //     }
+    //   }),
+    //   models.medico.findOrCreate({
+    //     where: {
+    //       id: "7"
+    //     },
+    //     defaults: {
+    //       nombre: "Leonel",
+    //       apellido: "Gatti",
+    //       email: "leogatti@email.com",
+    //       dni: 31973143,
+    //       especialidad: "Nutricion",
+    //       tiempo_trabajando: 30
+    //     }
+    //   })
+    // ])
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('medico', null, {});
   }
 };
--- src/database/seeders/20221123210057-primeros_pacientes.js
+++ src/database/seeders/20221123210057-primeros_pacientes.js
@@ -13,85 +13,145 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-    return Promise.all([
-      models.paciente.findOrCreate({
-        where: {
-          id: "1"
-        },
-        defaults: {
-          nombre: "Juan",
-          apellido: "Gonzalez",
-          email: "emailJuan@email.com",
-          dni: 25,
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "2"
-        },
-        defaults: {
-          nombre: "Pedro",
-          apellido: "Gomez",
-          email: "emailPedro@email.com",
-          dni: 67
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "3"
-        },
-        defaults: {
-          nombre: "Ana",
-          apellido: "Correa",
-          email: "anacorrea32@email.com",
-          dni: 33
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "4"
-        },
-        defaults: {
-          nombre: "Emilio",
-          apellido: "Lombardo",
-          email: "Emilioelmago@email.com",
-          dni: 71
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "5"
-        },
-        defaults: {
-          nombre: "Susana",
-          apellido: "Gimenez",
-          email: "lasugimenez@email.com",
-          dni: 32
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "6"
-        },
-        defaults: {
-          nombre: "Maria Silvana",
-          apellido: "Giacomo",
-          email: "msgiacomo@email.com",
-          dni: 23
-        }
-      }),
-      models.paciente.findOrCreate({
-        where: {
-          id: "7"
-        },
-        defaults: {
-          nombre: "Leonel",
-          apellido: "Gatti",
-          email: "leogatti@email.com",
-          dni: 36
-        }
-      })
-    ])
+    // return Promise.all([
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "1"
+    //     },
+    //     defaults: {
+    //       nombre: "Juan",
+    //       apellido: "Gonzalez",
+    //       email: "emailJuan@email.com",
+    //       dni: 25,
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "2"
+    //     },
+    //     defaults: {
+    //       nombre: "Pedro",
+    //       apellido: "Gomez",
+    //       email: "emailPedro@email.com",
+    //       dni: 67
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "3"
+    //     },
+    //     defaults: {
+    //       nombre: "Ana",
+    //       apellido: "Correa",
+    //       email: "anacorrea32@email.com",
+    //       dni: 33
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "4"
+    //     },
+    //     defaults: {
+    //       nombre: "Emilio",
+    //       apellido: "Lombardo",
+    //       email: "Emilioelmago@email.com",
+    //       dni: 71
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "5"
+    //     },
+    //     defaults: {
+    //       nombre: "Susana",
+    //       apellido: "Gimenez",
+    //       email: "lasugimenez@email.com",
+    //       dni: 32
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "6"
+    //     },
+    //     defaults: {
+    //       nombre: "Maria Silvana",
+    //       apellido: "Giacomo",
+    //       email: "msgiacomo@email.com",
+    //       dni: 23
+    //     }
+    //   }),
+    //   models.paciente.findOrCreate({
+    //     where: {
+    //       id: "7"
+    //     },
+    //     defaults: {
+    //       nombre: "Leonel",
+    //       apellido: "Gatti",
+    //       email: "leogatti@email.com",
+    //       dni: 36
+    //     }
+    //   })
+    // ])
+
+    return queryInterface.bulkInsert("paciente", [
+      {
+        nombre: "Juan",
+        apellido: "Gonzalez",
+        email: "emailJuan@email.com",
+        dni: 25,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Pedro",
+        apellido: "Gomez",
+        email: "emailPedro@email.com",
+        dni: 67,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Ana",
+        apellido: "Correa",
+        email: "anacorrea32@email.com",
+        dni: 33,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Emilio",
+        apellido: "Lombardo",
+        email: "Emilioelmago@email.com",
+        dni: 71,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Susana",
+        apellido: "Gimenez",
+        email: "lasugimenez@email.com",
+        dni: 32,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Maria Silvana",
+        apellido: "Giacomo",
+        email: "msgiacomo@email.com",
+        dni: 23,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nombre: "Leonel",
+        apellido: "Gatti",
+        email: "leogatti@email.com",
+        dni: 36,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ]
+    )
   },
 
   async down(queryInterface, Sequelize) {
@@ -101,5 +161,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('paciente', null, {});
   }
 };
